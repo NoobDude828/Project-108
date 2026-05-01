69aed1cfb0969b5a167da360f14ab6d9e39bc133
skarri-829 <sumitadhikari828@gmail.com>
2026-05-01 18:34:00 +0600

    no cache

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,21 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
